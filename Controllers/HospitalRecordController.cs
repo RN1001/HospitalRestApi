bd971ab454cc34e3e6c848f1822b8a81d5eca7c1
M Nazir <monazir1001@gmail.com>
2021-01-05 23:25:44 +0000

    Added hospital controller with 2 routes to test all patients and get patients by id routes.

--- Controllers/HospitalRecordController.cs
+++ Controllers/HospitalRecordController.cs
@@ -18,15 +18,27 @@ namespace HospitalRestApi.Controllers
                     "Raihaan",
                     "Nazir",
                     new System.DateTime(1993, 6, 9),
-                    new Doctor(1, "Eraj", "Arshad")
-                    ),
+                    new List<Doctor>()
+                    {
+                        new Doctor(1, "Eraj", "Arshad")
+                    },
+                    new List<Nurse>()
+                    {
+                        new Nurse(1, "Random", "Nurse")
+                    }
+                ),
                 new Patient(
                     2,
                     "Omair",
                     "Nazir",
                     new System.DateTime(1992, 4, 8),
-                    new Doctor(2, "Someone", "Somewhere")
-                    )
+                    new List<Doctor>()
+                    {
+                        new Doctor(2, "Doctor", "Cool"),
+                        new Doctor(3, "Doctor", "Big man")
+                    },
+                    new List<Nurse>()
+               )
             };
         }
 
