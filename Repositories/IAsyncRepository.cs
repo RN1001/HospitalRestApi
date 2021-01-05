a0b89758d0b5265b6f59c3a8994fb0a006295976
M Nazir <monazir1001@gmail.com>
2021-01-05 23:24:36 +0000

    Created empty Hospital DB Context and AsyncRepository as a base repository

---
+++ Repositories/IAsyncRepository.cs
@@ -0,0 +1,17 @@
+﻿using HospitalRestApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalRestApi.Repositories
+{
+    public interface IAsyncRepository<T> where T : BaseEntity
+    {
+        public Task<T> FindByIdAsync(int? id);
+        public Task<List<T>> FindAllAsync();
+        public Task<T> CreateAsync(T t);
+        public Task<T> UpdateAsync(int? id, T t);
+        public Task<T> DeleteAsync(int? id);
+    }
+}
