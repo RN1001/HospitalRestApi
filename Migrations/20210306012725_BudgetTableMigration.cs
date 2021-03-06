ab446f20f5706522b2e37f5d03d22543b6fd6273
M Nazir <monazir1001@gmail.com>
2021-03-06 01:31:37 +0000

    added budget migration to existing db

---
+++ Migrations/20210306012725_BudgetTableMigration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HospitalRestApi.Migrations
+{
+    public partial class BudgetTableMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Budgets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EquipmentBudget = table.Column<double>(type: "float", nullable: false),
+                    StaffingBudget = table.Column<double>(type: "float", nullable: false),
+                    TransportBudget = table.Column<double>(type: "float", nullable: false),
+                    DepartmentBudget = table.Column<double>(type: "float", nullable: false),
+                    TrainingBudget = table.Column<double>(type: "float", nullable: false),
+                    BudgetAddedDateTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Budgets", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Budgets");
+        }
+    }
+}
